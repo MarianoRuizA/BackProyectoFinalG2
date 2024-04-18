84077796bb56b3810220d1b32c4011ddad17c70f
lourdesreyna <reynalourdes91@gmail.com>
2024-04-17 23:22:54 -0300

    problemas con la conexion a la db

--- ProyectoFinalBackG2/index.js
+++ ProyectoFinalBackG2/index.js
@@ -12,6 +12,9 @@ app.use(cors());
 //se llama al port que esta en .env y a su ves se pasa un 2do puerto, si es que el del .env no esta disponible. Finalmente se levanta el servidor.
 const PORT = process.env.PORT || 5050;
 
+//en rutas esta el json.
+app.use(express.json());
+
 app.use("/api", reserverRouter)
 //endpoints de prueba.
 //http://localhost:4000/api/reservas
