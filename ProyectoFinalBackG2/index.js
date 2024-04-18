e49fd5c7188076af536727f1a19b637d1253b80f
lourdesreyna <reynalourdes91@gmail.com>
2024-04-17 23:59:02 -0300

    mongo db compass rechaza la conexion

--- ProyectoFinalBackG2/index.js
+++ ProyectoFinalBackG2/index.js
@@ -7,15 +7,14 @@ import connectDB from "./dataBase/db.js";
 
 const app = express(); //se guardan las configuraciones de express.
 
-app.use(cors());
-
 //se llama al port que esta en .env y a su ves se pasa un 2do puerto, si es que el del .env no esta disponible. Finalmente se levanta el servidor.
 const PORT = process.env.PORT || 5050;
 
+app.use(cors());
 //en rutas esta el json.
 app.use(express.json());
-
 app.use("/api", reserverRouter)
+
 //endpoints de prueba.
 //http://localhost:4000/api/reservas
 
