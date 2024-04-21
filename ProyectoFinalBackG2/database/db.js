bbb4eca4ca0eaa1444f82893c47dc38b68f39e40
LucianoCV01 <Lucianocosentino01@gmail.com>
2024-04-21 00:43:20 -0300

    merge fix

--- ProyectoFinalBackG2/database/db.js
+++ ProyectoFinalBackG2/database/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
-
-const URI = process.env.MONGO_URI;
-const DB = process.env.MONGO_DB;
+//conexion a la base de datos.
+const URI = process.env.MONGO_URI
+const DB = process.env.MONGO_DB
 
 const connectDB = async () => {
     try {
-      await mongoose.connect(`${URI}/${DB}`);
-      console.log("Conectado a la base de datos");
+      await mongoose.connect(`${URI}/${DB}`,)//esta propiedad permite facilitar la conexion con db.
+        console.log("conectado a la base de datos")
     } catch (error) {
         console.log("error")
     }
