a4a356e52d5e06150f1a3b6909c8a60360c24ac4
LucianoCV01 <Lucianocosentino01@gmail.com>
2024-05-19 21:19:51 -0300

    correcion rutas privadas

--- ProyectoFinalBackG2/router/private.router.js
+++ ProyectoFinalBackG2/router/private.router.js
@@ -12,4 +12,15 @@ router.get("/admin", (req, res) => {
   });
 });
 
+// Obtener usuarios
+router.get("/usuarios", usuarioController.getAllUsuarios);
+// Modificar usuario
+router.patch("/usuarios/:id", usuarioController.updateUsuario);
+// Eliminar usuario
+router.delete("/usuarios/delete/:id", usuarioController.deleteUsuario);
+// Eliminar reserva
+router.delete("/reservas/:id", reservaControllers.eliminarReserva)
+// Modificar reserva
+router.patch("/reservas/:id", reservaControllers.modificarReserva);
+
 export default router;
--- ProyectoFinalBackG2/router/reservaRouter.js
+++ ProyectoFinalBackG2/router/reservaRouter.js
@@ -7,9 +7,5 @@ const router = express.Router()
 router.post("/crear", reservaControllers.crearReserva);
 // Traer reserva
 router.get("/reservas", reservaControllers.getAllReserva)
-// Eliminar reserva
-router.delete("/reservas/:id", reservaControllers.eliminarReserva)
-// Modificar reserva
-router.patch("/reservas/:id", reservaControllers.modificarReserva);
 
 export default router
